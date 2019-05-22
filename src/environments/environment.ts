7d1609bbeb61b084ff390788ee29d0996c04372c
Diego Reymy <diego.h.aguilar@accenture.com>
2019-05-22 12:45:01 -0300

    Configurando base de datos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDuQmQXaBGg6arbn5m4KuDd9HevsWm-c_I',
-    authDomain: 'ven-app-40809.firebaseapp.com',
-    databaseURL: 'https://ven-app-40809.firebaseio.com/',
-    projectId: 'ven-app-40809',
+    apiKey: 'process.env.apiKey',
+    authDomain: 'process.env.authDomain',
+    databaseURL: 'process.env.databaseURL',
+    projectId: 'process.env.projectId',
     storageBucket: '',
-    messagingSenderId: '275258311939'
+    messagingSenderId: 'process.env.messagingSenderId'
   }
 };
